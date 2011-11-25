531b48551fa8db3fe9609ab4cd185916384cf39f
Bartek Bułat <bartek.bulat@gmail.com>
2011-11-25 23:31:12 +0100

    Dodanie funkcji pobierania wartości estymowanej wcześniej.

--- SugarControl/src/sugar/control/core/BloodGlucoseEstimator.java
+++ SugarControl/src/sugar/control/core/BloodGlucoseEstimator.java
@@ -1,5 +1,6 @@
 package sugar.control.core;
 
+import android.text.format.Time;
 import sugar.control.utils.Spline;
 
 /**
@@ -12,6 +13,9 @@ public class BloodGlucoseEstimator {
   
   private double coeff; // Stosunek ilości glukozy przy pomiarze do 50g
   private Spline GTTCurve = null;
+  private double lastValues[];
+  private int mH, mM;
+  private Time tt = new Time();
 
   public BloodGlucoseEstimator() {
   }
@@ -31,19 +35,59 @@ public class BloodGlucoseEstimator {
     if (GTTCurve == null) {
       throw new RuntimeException("Wczytaj najpierw dane krzywej glikemicznej.");
     }
+    tt.setToNow();
+    int _mH = tt.hour;
+    int _mM = tt.minute;
+    
+    
     double[] output = new double[howlong];
     double first = GTTCurve.val(0);
-    double c = coeff*ig/100;
+    double c = coeff*ig/100;    
     for(int i = 0; i < howlong; ++i) {
-      output[i] = getlastValue(i) + (GTTCurve.val(i) - first)*c;
+      output[i] = getlastValue(i,_mH, _mM) + (GTTCurve.val(i) - first)*c;
     }
 
     return output;
   }
+  
+  public void saveEstimatimation(double ig, int howlong) {
+    if( howlong > 360) {
+      throw new IllegalArgumentException("Nie można estymować poziomu cukru dłużej niż 360 minut");
+    }
+    if (GTTCurve == null) {
+      throw new RuntimeException("Wczytaj najpierw dane krzywej glikemicznej.");
+    }
+    tt.setToNow();
+    int _mH = tt.hour;
+    int _mM = tt.minute;
+    
+    
+    double[] output = new double[howlong];
+    double first = GTTCurve.val(0);
+    double c = coeff*ig/100;    
+    for(int i = 0; i < howlong; ++i) {
+      output[i] = getlastValue(i,_mH, _mM) + (GTTCurve.val(i) - first)*c;
+    }
+    
+    lastValues = new double[howlong];
+    for(int i = 0; i < howlong; ++i) {
+      lastValues[i] = output[i];
+    }
+  }
 
-  // TODO
   private double getlastValue(int i) {
-    return 80;
-    //throw new RuntimeException("metoda niezaimplementowana");
+    tt.setToNow();
+    int _H = tt.hour;
+    int _M = tt.minute;
+    return getlastValue(i, _H, _M);
+  }
+  
+  private double getlastValue(int i, int _H, int _M) {
+    i = i + (_H - mH)*60 + mM - _M;
+    if (i >= lastValues.length) {
+      return lastValues[lastValues.length -1];
+    } else {
+      return lastValues[i];
+    }
   }
 }
