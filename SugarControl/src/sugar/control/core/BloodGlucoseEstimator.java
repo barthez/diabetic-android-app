19470f3ccd0ba35ab7baa556affaca932d0713a0
Bartek Bułat <bartek.bulat@gmail.com>
2011-11-26 01:56:46 +0100

    JavaDOC do estymatora

--- SugarControl/src/sugar/control/core/BloodGlucoseEstimator.java
+++ SugarControl/src/sugar/control/core/BloodGlucoseEstimator.java
@@ -16,7 +16,11 @@ public class BloodGlucoseEstimator {
   private Spline GTTCurve = null;
   private double lastValues[];
   private Time estimationStart = new Time();
-
+  
+  /**
+   * Ustaw współczynnik wysiłku fizycznego
+   * @param activCoff Współczynnik wysiłku, im większy tym szybciej rośnie/spada cukier
+   */
   public void setActivCoff(double activCoff) {
     this.activCoff = activCoff;
   } 
@@ -34,6 +38,11 @@ public class BloodGlucoseEstimator {
   }
   
   static BloodGlucoseEstimator instance = null;
+  
+  /**
+   * Pobierz instancję Singletonu klasy BloodGlucoseEstimator
+   * @return Instancja Singletonu
+   */
   public static BloodGlucoseEstimator getInstance() {
     if (instance == null) {
       instance = new BloodGlucoseEstimator();
@@ -41,7 +50,11 @@ public class BloodGlucoseEstimator {
     return instance;
   }
   
-
+  /**
+   * Ustaw krzywą glikemiczną
+   * @param data tablica double z wartościami pomiarów w czasie 0 30 60 120 180 240 300 300 min od spożycia preparatu z glukozy
+   * @param sW Ilość glukozy w preparacie w gramach
+   */
   public void setGTTCurve(double[] data, double sW) {
     if (data.length != 8) {
       throw new IllegalArgumentException("Dane musza mieć 8 pomiarów w czasie 0, 0.5, 1, 2, 3, 4, 5, 6 godziny po podaniu glukozy.");
@@ -50,6 +63,10 @@ public class BloodGlucoseEstimator {
     coeff = sW/50;
   }
   
+  /**
+   * Ustaw aktualną, zmierzoną wartość glukozy we krwi
+   * @param value Wartość pomiaru w mg/dl
+   */
   public void setGlucoseValue(double value) {
     if (lastValues == null) {
       lastValues = new double[1];
@@ -62,6 +79,12 @@ public class BloodGlucoseEstimator {
     }
   }
 
+  /**
+   * Oblicz estymowane wartości poziomu cukru we krwi przez zadany czas
+   * @param ig Indeks glikemiczny potrawy
+   * @param howlong Długość estymaczji w minutach
+   * @return Tablica wartości poziomu cukru co minutę
+   */
   public double[] estimate(double ig, int howlong) {
     if (GTTCurve == null) {
       throw new RuntimeException("Wczytaj najpierw dane krzywej glikemicznej.");
@@ -82,6 +105,11 @@ public class BloodGlucoseEstimator {
     return output;
   }
   
+  /**
+   * Tak jak estimate ale wynik jest zapisywany
+   * @param ig Indeks glikemiczny potrawy
+   * @param howlong Długość estymaczji w minutach
+   */
   public void saveEstimatimation(double ig, int howlong) {
     double output[] = estimate(ig, howlong);    
     
@@ -93,13 +121,24 @@ public class BloodGlucoseEstimator {
     }
   }
 
-  private double getlastValue(int i) {
+  /**
+   * Pobiera wartość poziomu cukru w i minut od teraz
+   * @param i Ilość minut od teraz
+   * @return Wartość poziomu cukru w mg/dl
+   */
+  public double getlastValue(int i) {
     
     Time tt = new Time();
     tt.setToNow();
     return getlastValue(i, tt);
   }
   
+  /**
+   * Pobiera wartość poziomu cukru w i minut od czasu t
+   * @param i Ilość minut od czasu t
+   * @param t Czas od którego liczymy przesunięcie czasowe
+   * @return Wartość poziomu cukru w mg/dl
+   */
   private double getlastValue(int i, Time t) {
     i = i + (t.hour - estimationStart.hour)*60 + t.minute - estimationStart.minute;
     try {
